22276edb65b5caca1bc32a2b604d704ec7d93fa3
Jenn <83610597+JenniferCortina@users.noreply.github.com>
2021-08-22 12:19:20 -0400

    Add files via upload

---
+++ Week9Day2ClassNotes/src/main/java/com/tts/week9day2/Week9day2Application.java
@@ -0,0 +1,13 @@
+package com.tts.week9day2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week9day2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week9day2Application.class, args);
+	}
+
+}
---
+++ Week9Day2ClassNotes/src/main/java/com/tts/week9day2/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.tts.week9day2.repository;
+
+import com.tts.week9day2.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+}
